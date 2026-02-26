297090dc117e1f739d34391fc2d683ae22fe618e
レ カオ クオン <lc_cuong@ad01.corp.toadenshi.co.jp>
2026-02-26 17:50:13 +0900

    up tieeps

--- session_32th_260206/task9/scrip.js
+++ session_32th_260206/task9/scrip.js
@@ -1,10 +1,9 @@
 const form = document.getElementById("form");
 const input = document.getElementById("form_input");
 
-const todoList = document.getElementById("todo-list");
-
 class todoApp {
-    constructor() {
+    constructor(todoContainer) {
+        this.todoList = document.getElementById(todoContainer);
         this.todoArr = [];
         this.id = (() => {
             let i = 0;
@@ -12,52 +11,78 @@ class todoApp {
         })();
     }
 
-    addWork() {
-        const inputValue = input.value.trim();
-        input.value = "";
-        if (!inputValue) return;
+    addWork(workName) {
+        if (!workName) return;
         const work = {
-            id: id(),
+            id: this.id(),
             done: false,
-            name: inputValue,
+            name: workName,
         };
+
+        this.todoArr.push(work);
+        console.log(this.todoArr);
+
+        this.renderWork(this.todoArr);
+    }
+
+    renderWork(renderData) {
+        this.todoList.innerHTML = "";
+        renderData.forEach((work) => {
+            const li = document.createElement("li");
+            li.id = work.id;
+
+            const liContnet = document.createElement("span");
+            liContnet.innerText = work.name;
+
+            const doneBtn = document.createElement("button");
+            doneBtn.innerText = "Done";
+            doneBtn.classList.add("done-btn");
+            work.done && doneBtn.classList.add("done-btn-green");
+
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerText = "✕";
+
+            li.appendChild(liContnet);
+            li.appendChild(doneBtn);
+            li.appendChild(deleteBtn);
+
+            doneBtn.addEventListener("click", (e) => this.doneWork(e));
+            deleteBtn.addEventListener("click", (e) => this.deleteWork(e));
+            this.todoList.appendChild(li);
+        });
+    }
+
+    doneWork(workElement) {
+        const li = workElement.target.closest("li");
+        const index = this.todoArr.findIndex((w) => w.id === Number(li.id));
+
+        this.todoArr[index].done = this.todoArr[index].done === true ? false : true;
+        workElement.target.classList.toggle("done-btn-green", this.todoArr[index].done);
+    }
+
+    deleteWork(workElement) {
+        const li = workElement.target.closest("li");
+        const index = this.todoArr.findIndex((w) => w.id === Number(li.id));
+        this.todoArr.splice(index, 1);
+        li.remove();
     }
 }
 
-const todoArr = [];
-const id = (() => {
-    let i = 0;
-    return () => ++i;
-})();
+const todo1 = new todoApp("todo-list");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const inputValue = input.value.trim();
     input.value = "";
-    if (!inputValue) return;
+    todo1.addWork(inputValue);
 
-    todoArr.push(work);
+    // if (!inputValue) return;
+    // doneBtn.addEventListener("click", (e) => {
+    //     e.target.classList.toggle("done-btn-green");
+    // });
 
-    const li = document.createElement("li");
-    li.innerText = inputValue + " ";
-
-    const doneBtn = document.createElement("button");
-    doneBtn.innerText = "Done";
-    doneBtn.classList.add("done-btn");
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "✕";
-
-    li.appendChild(doneBtn);
-    li.appendChild(deleteBtn);
-    todoList.appendChild(li);
-
-    doneBtn.addEventListener("click", (e) => {
-        e.target.classList.toggle("done-btn-green");
-    });
-
-    deleteBtn.addEventListener("click", (e) => {
-        li.remove();
-    });
+    // deleteBtn.addEventListener("click", (e) => {
+    //     li.remove();
+    // });
 });
